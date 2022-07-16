0533d31bce090b836b7c23bd8035c1931ed1d006
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2022-07-16 21:09:00 +0530

    Update First.java

--- First.java
+++ First.java
@@ -2,6 +2,7 @@ public class First{
     public static void main(String args[]){
         System.out.println("Hello,Good Morning All of You");
         System.out.println("Good");
+        System.out.println("BAD");
         
     }
 }
